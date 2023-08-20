e5a8e00834d7076339a163ad276541985b995fe5
Brenton Bostick <bostick@gmail.com>
2023-08-20 09:22:16 -0400

    separate jar and dylib

--- build.sh
+++ build.sh
@@ -519,19 +519,19 @@ host-jar()
     $CMAKE $VARIANT -H. -B$CACHE_DIR -DCMAKE_BUILD_TYPE=$BUILD_TYPE
     $CMAKE --build $CACHE_DIR $BUILD_CONCURRENCY
     # Package everything
-    cp -f $CACHE_DIR/lib/libzt.* $JAVA_JAR_DIR
     cd $JAVA_JAR_DIR
     export JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
     javac -Xlint:all com/zerotier/sockets/*.java
 
-    jar cf libzt-$PKG_VERSION.jar $SHARED_LIB_NAME com/zerotier/sockets/*.class
-    rm -rf com $SHARED_LIB_NAME
+    jar cf libzt-$PKG_VERSION.jar com/zerotier/sockets/*.class
+
     cd -
     # Copy JAR to dist/
     echo -e "\nContents of JAR:\n"
     jar tf $JAVA_JAR_DIR/*.jar
     echo -e
     mv $JAVA_JAR_DIR/*.jar $PKG_OUTPUT_DIR
+    cp -f $CACHE_DIR/lib/libzt.* $PKG_OUTPUT_DIR
     echo -e "\n - Build cache  : $CACHE_DIR\n - Build output : $BUILD_OUTPUT_DIR\n"
     $TREE $TARGET_BUILD_DIR
 
